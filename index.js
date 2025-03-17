8db31b2508cab89daa8dd4f975e94cd571f75b1f
Lumikin <lumikomio04@gmail.com>
2025-03-17 12:48:35 -0300

    Salve

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 let Usuario = prompt("Qual o seu nome?"); // variavel que armazena o nome do usuario sendo prompt uma função que pede um valor ao usuario
-let 
+
+alert(' Ola bem vindo ' + Usuario); // alerta que mostra uma mensagem de boas vindas ao usuario
+
